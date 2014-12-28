92727e9002cc167e7099f5b18054cc7d75cf11fe
Connie Chen <cchen@twilio.com>
2014-12-27 20:35:47 -0800

    applicative

--- exercises/src/main/scala/fpinscala/applicative/Applicative.scala
+++ exercises/src/main/scala/fpinscala/applicative/Applicative.scala
@@ -9,18 +9,34 @@ import monoids._
 
 trait Applicative[F[_]] extends Functor[F] {
 
-  def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = ???
+  def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = {
+    //val f = map(fa)(f.curried)
+    apply(map(fa)(f.curried))(fb)
+  }
 
-  def apply[A,B](fab: F[A => B])(fa: F[A]): F[B] = ???
+  def map3[A,B,C,D](fa: F[A],
+                    fb: F[B],
+                    fc: F[C])(f: (A, B, C) => D): F[D] = {
+
+    apply(map(fa)(map(fb)(f.curried)))(fc)
+  }
+
+  def apply[A,B](fab: F[A => B])(fa: F[A]): F[B] = {
+    map2(fa, fab)((a, atob) => atob(a))
+  }
 
   def unit[A](a: => A): F[A]
 
   def map[A,B](fa: F[A])(f: A => B): F[B] =
     apply(unit(f))(fa)
 
-  def sequence[A](fas: List[F[A]]): F[List[A]] = ???
+  def sequence[A](fas: List[F[A]]): F[List[A]] = {
+    traverse(fas)(fa => fa)
+  }
 
-  def traverse[A,B](as: List[A])(f: A => F[B]): F[List[B]] = ???
+  def traverse[A,B](as: List[A])(f: A => F[B]): F[List[B]] = {
+    as.foldRight(unit(List[B]()))((b, flb) => map2(f(b), flb)(_ :: _))
+  }
 
   def replicateM[A](n: Int, fa: F[A]): F[List[A]] = ???
 
